08a34e0eb3448ccd05a3b7a9ce19aa599e21aeed
Paul Zietsman <phzietsman@gmail.com>
2021-05-11 19:40:41 +0200

    This object does not have an attribute named "outputs".

--- workspace_provisioner/output.tf
+++ workspace_provisioner/output.tf
@@ -5,7 +5,7 @@ data "tfe_workspace_ids" "all" {
 
 locals {
 
-  managed_workspace_ids = [for _, workspace in module.workspace : workspace.outputs["workspace_id"] if workspace.outputs["workspace_id"] != "ws-null"]
+  managed_workspace_ids = [for _, workspace in module.workspace : workspace.workspace_id if workspace.workspace_id != "ws-null"]
   all_workspace_ids     = [for _, workspace_id in data.tfe_workspace_ids.all.ids : workspace_id]
 
   unmanaged_workspace_ids   = setsubtract(local.all_workspace_ids, local.managed_workspace_ids)
